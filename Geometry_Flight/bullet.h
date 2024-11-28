64f76870de51629484cb54570c437e8ea689ff7b
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-11-28 21:02:15 +0900

    클래스 Player 생성

--- Geometry_Flight/bullet.h
+++ Geometry_Flight/bullet.h
@@ -33,3 +33,38 @@ public:
     }
 };
 
+//플레이어 클래스
+class Player : public Model
+{
+public:
+    // 각 축 이동속도
+    float xSpeed = 0.0f;
+    float ySpeed = 0.0f;
+    float zSpeed = 0.0f;
+    //시작 생명 3개
+    int life = 3;
+    //
+    Player(const Model model) : Model(model)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dis(0.005f, 0.995f);
+        // 색상 초기화
+        colors.resize(vertex_count);
+        for (size_t i = 0; i < vertex_count; i++) {
+            colors[i] = glm::vec3(dis(gen), dis(gen), dis(gen));  // 흰색으로 초기화
+        }
+
+        //위아래로 흔들림
+        //fill
+    }
+
+    void update()
+    {
+        // 매 프레임마다 이동
+        positionX += xSpeed;
+        positionX += ySpeed;
+        positionZ += zSpeed;
+    }
+};
+
